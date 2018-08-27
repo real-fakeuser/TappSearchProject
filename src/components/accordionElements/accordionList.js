314b9c5ef3c9187df421782daee7708d6160bbd3
realfakeuser <tim.niewerth@tobit.software>
2018-08-27 11:59:12 +0200

    keyup event added

--- src/components/accordionElements/accordionList.js
+++ src/components/accordionElements/accordionList.js
@@ -25,6 +25,7 @@ export default class siteList {
             </div>
         </div>
                     `);
+        
         return accordionForm;
     }
 }
--- src/utils/addEventListener.js
+++ src/utils/addEventListener.js
@@ -7,3 +7,7 @@
 import frmLogic from '../components/formContainer/sendFrm';
 let objfrmLogic = new frmLogic;
 document.querySelector('#sendForm').addEventListener("click", objfrmLogic.processUserInput);
+
+import searchEngine from '../components/listContainer/searchengine';
+let objSearch = new searchEngine;
+document.querySelector('#SiteSearch').onkeyup = objSearch.keyUpSearch;
