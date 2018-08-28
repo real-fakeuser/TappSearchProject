9f8fd79bbf58ce5e8b9f8d0aff18f9902073f723
realfakeuser <tim.niewerth@tobit.software>
2018-08-28 15:24:23 +0200

    form new structure

--- src/components/formContainer/getUserInput.js
+++ src/components/formContainer/getUserInput.js
@@ -1,23 +1,27 @@
 export default class frmAccess {
-    constructor() {}
+    constructor() {
+        this.field = ['name', 'address', 'eMailAddr', 'comment'];
+        this.field['name']        = document.querySelector('#frmName');
+        this.field['address']     = document.querySelector('#frmAddress');
+        this.field['eMailAddr']   = document.querySelector('#frmMailAddr');
+        this.field['comment']     = document.querySelector('#frmComment');
+        
+    }
 
     get = () => {
 
         let data = ['name' , 'address' , 'eMailAddr' ,  'comment'];
-        data['name']        =   document.querySelector('#frmName').value;
-        data['address']     =   document.querySelector('#frmAddress').value;
-        data['eMailAddr']   =   document.querySelector('#frmMailAddr').value;
-        data['comment']     =   document.querySelector('#frmComment').value;
+        data['name']        =   this.field['name'].value;
+        data['address']     =   this.field['address'].value;
+        data['eMailAddr']   =   this.field['eMailAddr'].value;
+        data['comment']     =   this.field['comment'].value;
         
         return data;
     }
 
     set = () => {
-        document.querySelector('#frmName').value        =   "";
-        document.querySelector('#frmAddress').value     =   "";
-        document.querySelector('#frmMailAddr').value    =   "";
-        document.querySelector('#frmComment').value     =   "";
-
-        return;
+        this.field.forEach(element => {
+            this.field[element].value = '';
+        });
     }
 }
