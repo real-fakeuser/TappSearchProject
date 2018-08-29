2aafc97464617bcd2d6c931386f756ca40054633
realfakeuser <tim.niewerth@tobit.software>
2018-08-29 09:08:32 +0200

    try catch added

--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
-/*import searchEngine from './components/listContainer/searchengine';
-import searchBar from './components/searchbar/container';
-import accordionList from './components/accordionElements/accordionList';
-import showMoreBtn from './components/accordionElements/showMoreBtn';*/
 import formContainer from './components/formContainer/formContainer';
 import listContainer from './components/listContainer/listContainer';
 
 const init = async () => {
-    await chayns.ready;
-//############### Form
-      let frmBaseElement = document.querySelector('#addForm');
-      let frmContainer = new formContainer(frmBaseElement);
-     
 
-     
-//###############
+      chayns.ready.then(function() {
+            console.log('Chayns is ready, environment loaded', chayns.env);
+            //############### Form
+                  let frmBaseElement = document.querySelector('#addForm');
+                  let frmContainer = new formContainer(frmBaseElement);
+            //###############
+            //############### create wrapper
+                  let listBaseElement = document.querySelector('#resultList');
+                  let lstContainer = new listContainer(listBaseElement);
+            //##############################
 
-//############### create wrapper
-      let listBaseElement = document.querySelector('#resultList');
-      let lstContainer = new listContainer(listBaseElement);
-
-
-     /* let objListWrapper = new accordionList();
-      let objSearchBar = new searchBar();
-      let objShowMore = new showMoreBtn();
-      let listWrapper = document.querySelector('#resultList');
-      listWrapper.appendChild(objListWrapper.searchAccordion());                                            //creates the basic structure of the accordion
-      document.querySelector('#showMore').appendChild(objShowMore.block(new searchEngine().enlargeList));   //adds the show more button and event listener
-      let searchBox = document.querySelector('#searchbar');
-      searchBox.appendChild(objSearchBar.searchBarBox(new searchEngine().keyUpSearch));                     //creates searchbar and key up event
-      new searchEngine().search('Tobit');                                                                   //Creates a search request*/
-
-
-
-//##############################
-
-
+          }).catch(function() { 
+            console.log('No chayns environment found');
+          }).then(function () { 
+            console.log('Will always be executed');
+          });
+          
 };
-
 init();
